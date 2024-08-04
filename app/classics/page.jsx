9415644f1a6e0eeda1622d629d0a7fc1da434c83
BoneHeadForever <161755588+BoneHeadForever@users.noreply.github.com>
2024-08-04 23:33:05 +0300

    Update page.jsx

--- app/classics/page.jsx
+++ app/classics/page.jsx
@@ -2,20 +2,21 @@ import { FeedbackForm } from 'components/feedback-form';
 import { Markdown } from '../../components/markdown';
 
 export const metadata = {
-    title: 'Classics'
+    title: 'Geri Bildirim'
 };
 
 const explainer = `
-Some classic (and much-loved) Netlify features were born when most sites we hosted were fully static.
-For example, [Netlify Forms](https://docs.netlify.com/forms/setup/) do their magic based on automatic detection of specially-marked form tags in static HTML files. 
+Luritz
+Merhaba! Ben Luritz, tasarım ve kullanıcı deneyimi konusunda uzmanlaşmış bir profesyonelim. Şıklığı ve işlevselliği bir araya getirerek projelerinizi görsel olarak mükemmel hale getiriyorum.
 
-This has [required some adjustments](https://docs.netlify.com/forms/setup/#javascript-forms) for the age of SPA and SSR. 
-With modern Next.js versions, no page is truly static: as a developer, you can revalidate any page. However, you can still use our forms.
+Ne Yapıyorum?
+Tasarım ve kullanıcı deneyimi odaklı projeler geliştiriyorum. Amacım, estetik ve işlevsel açıdan mükemmel sonuçlar elde etmek ve kullanıcılarınızın beklentilerini karşılamaktır.
 
-Below is a simple form using \`fetch\` to submit its data to Netlify rather than using full-page navigation. To be detected, form tags must be hosted in static files -
-and \`public/__forms.html\` exists just for this purpose.
+Vizyonum
+Tasarım ve estetik anlayışımı kullanarak, projelerinizda fark yaratmak ve kullanıcı deneyimini en üst düzeye çıkarmak.
 
-Deploy this site to your Netlify account, [enable the forms feature in the UI](https://docs.netlify.com/forms/setup/#enable-form-detection), trigger a build and you can start collecting submissions.
+Misyonum
+Kaliteli ve etkili tasarımlar sunarak projelerinizi başarılı kılmak. Her projede yenilikçi ve yaratıcı çözümler sunmak için çalışıyorum.
 `;
 
 export default async function Page() {
